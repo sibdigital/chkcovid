24fe0b76c2e5a48dde97d7269f1af1d5c4b0e223
ts808 <tmd979@gmail.com>
2020-04-05 22:18:57 +0800

    Изменение механизма поиска

--- src/main/java/ru/sibdigital/chkcovid/repository/OrganizationRepo.java
+++ src/main/java/ru/sibdigital/chkcovid/repository/OrganizationRepo.java
@@ -1,20 +1,15 @@
 package ru.sibdigital.chkcovid.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.sibdigital.chkcovid.domain.Organization;
 
 import java.util.List;
 
 @Repository
-public interface OrganizationRepo extends  JpaRepository<Organization, Integer>{
+public interface OrganizationRepo extends  JpaRepository<Organization, Integer> {
 
-    List<Organization> findAllByInn(String inn);
     List<Organization> findAllByInnAndLastnameAndFirstname(String inn, String lastname, String firstname);
     List<Organization> findAllByInnAndLastnameAndFirstnameAndPatronymic(String inn, String lastname, String firstname, String patronymic);
-    List<Organization> findAllByLastnameAndFirstname(String lastname, String firstname);
-    List<Organization> findAllByLastnameAndFirstnameAndPatronymic(String lastname, String firstname, String patronymic);
 
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -8,31 +8,46 @@ function submitForm(e) {
     organization.patronymic = $("#patronymic").val();
     organization.inn = $("#inn").val();
 
-    $.ajax({
-        type: "POST",
-        contentType: "application/json",
-        url: "/",
-        data: JSON.stringify(organization),
-        success: function (data) {
-            $("#orgTable tbody").empty();
+    var valid = true;
 
-            if (data.length === 0) {
-                $("#noResults").attr("style", "display:block")
-            } else {
-                $("#noResults").attr("style", "display:none")
+    for (var key in organization) {
+        if (organization[key] === "" && key !== "patronymic") {
+            $("#" + key).addClass("is-invalid");
+            valid = false;
+        }
+    }
 
-                for (var i = 0; i < data.length; i++) {
-                    var html =
-                        "<tr>" +
-                        "<td class=\"text-center\">" + data[i].lastname + " " + data[i].firstname + " " + data[i].patronymic + "</td>" +
-                        "<td class=\"text-center\">" + data[i].inn + "</td>" +
-                        "<td class=\"text-center\">" + data[i].organizationName + "</td>" +
-                        "</tr>";
-                    $("#orgTable").append(html);
+    if (valid) {
+        for (var key in organization) {
+            $("#" + key).removeClass("is-invalid");
+        }
+
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: "/",
+            data: JSON.stringify(organization),
+            success: function (data) {
+                $("#orgTable tbody").empty();
+
+                if (data.length === 0) {
+                    $("#noResults").attr("style", "display:block")
+                } else {
+                    $("#noResults").attr("style", "display:none")
+
+                    for (var i = 0; i < data.length; i++) {
+                        var html =
+                            "<tr>" +
+                            "<td class=\"text-center\">" + data[i].lastname + " " + data[i].firstname + " " + data[i].patronymic + "</td>" +
+                            "<td class=\"text-center\">" + data[i].inn + "</td>" +
+                            "<td class=\"text-center\">" + data[i].organizationName + "</td>" +
+                            "</tr>";
+                        $("#orgTable").append(html);
+                    }
                 }
             }
-        }
-    });
+        });
+    }
 }
 
 $("#submit").on("mousedown", function (e) {
